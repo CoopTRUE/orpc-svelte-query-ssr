a0e3e2dde5be25eda45ffa0d1e505f43ae2eda56
CoopTRUE <coopdog4545@gmail.com>
2025-08-06 09:25:52 -0700

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+import svelteConfig from './svelte.config.js'
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import prettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import svelte from 'eslint-plugin-svelte'
+import globals from 'globals'
+import { fileURLToPath } from 'node:url'
+import ts from 'typescript-eslint'
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.strict,
+  ...ts.configs.stylistic,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  ...pluginQuery.configs['flat/recommended'],
+  {
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': ['warn', { groups: [['.*']] }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'svelte/sort-attributes': 'warn',
+      'svelte/block-lang': ['warn', { script: 'ts', style: 'postcss' }],
+      'svelte/require-each-key': 'off',
+      'svelte/require-optimized-style-attribute': 'warn',
+      'svelte/consistent-selector-style': 'warn',
+      'svelte/html-self-closing': 'warn',
+      'svelte/html-closing-bracket-new-line': 'warn',
+      'svelte/derived-has-same-inputs-outputs': 'warn',
+      'svelte/prefer-class-directive': 'warn',
+      'svelte/prefer-style-directive': 'warn',
+      'svelte/require-event-prefix': 'warn',
+      'svelte/spaced-html-comment': 'warn',
+    },
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+  }
+)
